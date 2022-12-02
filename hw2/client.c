3372c1d9b46a95a7e3ba1f0a0eb11ae8f0a86506
bobo <bobo511326@gmail.com>
2022-12-02 15:26:40 +0800

    modify

--- hw2/client.c
+++ hw2/client.c
@@ -13,18 +13,21 @@ int main(int argc, char *argv[])
     int n;
     char buf[BUFSIZE];
     
-    if(argc != 4)
+    if(argc != 4){
         errexit("Usage: %s host_address host_port host_message\n",argv[0]);
+    }
     
     connfd = connectsock(argv[1],argv[2],"tcp");
     
-    if((n = write(connfd, argv[3],strlen(argv[3]))) == -1)
+    if((n = write(connfd, argv[3],strlen(argv[3]))) == -1){
         errexit("Error: write()\n");
+    }
     
     memset(buf, 0, BUFSIZE);
     
-    if((n = read(connfd, buf, BUFSIZE)) == -1)
+    if((n = read(connfd, buf, BUFSIZE)) == -1){
         errexit("Error: read()\n");
+    } 
     
     printf("%s\n", buf);
     
