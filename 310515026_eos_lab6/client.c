a11e995866e38249e2e9636ba5cfe5bdfd65e0d0
bobo <bobo511326@gmail.com>
2022-12-15 17:25:25 +0800

    lab6

--- 310515026_eos_lab6/client.c
+++ 310515026_eos_lab6/client.c
@@ -1,42 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "socket_utils.h"
 
 int main(int argc, char **argv)
 {
-  int conn_fd, msg_cnt, i;
-  char action = 0;
-  char msg_buf[50];
+  int connfd, n, i;
+  char msg_buf[100];
 
-  if (argc != 6)
-  {
+  if(argc != 6){
     printf("Usage: %s <host> <port> <deposit/withdraw> <amount> <times>\n", argv[0]);
     exit(-1);
   }
 
-  if (strcmp(argv[3], "deposit") == 0)
-  {
-    action = 'D';
-  }
-  else if (strcmp(argv[3], "withdraw") == 0)
-  {
-    action = 'W';
-  }
-  else
-  {
-    sprintf(msg_buf, "invalid action %s\n", argv[3]);
-    perror(msg_buf);
-    exit(-1);
-  }
-  msg_cnt = sprintf(msg_buf, "%c,%s", action, argv[4]);
+  connfd = createClientSock(argv[1], atoi(argv[2]), TRANSPORT_TYPE_TCP);
 
-  for (i = 0; i < atoi(argv[5]); i++)
-  {
-    conn_fd = createClientSock(argv[1], atoi(argv[2]), TRANSPORT_TYPE_TCP);
-    write(conn_fd, msg_buf, msg_cnt);
-    close(conn_fd);
-  }
+  for(i = 0; i < atoi(argv[5]); i++){
+    memset(msg_buf, 0, 100);
+    strcpy(msg_buf, argv[3]);
+    strcat(msg_buf, " ");
+    strcat(msg_buf, argv[4]);
 
+    if((n = write(connfd , msg_buf , strlen(msg_buf) + 1)) == -1){
+      printf("Error : write ()\n");
+    }
+
+    printf("[Client] Send: %s\n", msg_buf);
+  } 
+
+  close(connfd); 
   return 0;
-} 
+}
--- 310515026_eos_lab6/server.c
+++ 310515026_eos_lab6/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>  // fork()
 #include <signal.h>  // signal()
 #include <pthread.h> // pthread_create, pthread_join, etc.
@@ -7,107 +8,21 @@
 #include <sys/wait.h>
 #include <sys/sem.h>
 #include "socket_utils.h"
+
 #define BUFSIZE 1024
-#define NUM_THREADS 8
-#define SEM_KEY 12345678
+#define NUM_THREADS 10
+
+#define SEM_KEY 1234
 #define SEM_MODE 0666
 
-int total_amount = 0;
-void connectCallback(int conn_fd);
-
 int sem;
-int P(int s);
-int V(int s);
+int amount = 0;
 
-int sock_fd;
-int stop=0;
-void intHandler(int signum);
-
-int main(int argc, char **argv)
+typedef struct
 {
-  int conn_fd;
-  struct sockaddr_in cln_addr;
-  socklen_t sLen = sizeof(cln_addr);
-  pthread_t thread;
-
-  if (argc != 2)
-  {
-    printf("Usage: %s <port>\n", argv[0]);
-    exit(-1);
-  }
-
-  sock_fd = createServerSock(atoi(argv[1]), TRANSPORT_TYPE_TCP);
-  if (sock_fd < 0)
-  {
-    perror("Error create socket\n");
-    exit(-1);
-  }
-
-  // Creating semaphore
-  sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | SEM_MODE);
-  // sem = semget(SEM_KEY, 1, IPC_CREAT | SEM_MODE);
-  if (sem < 0){
-    perror("Error create semaphore\n");
-    exit(-1);
-  }
-  printf("sem created\n");
-  if( semctl(sem, 0, SETVAL, 1) < 0){
-    perror("Error semctl\n");
-    exit(-1);
-  }
-
-  signal(SIGINT, intHandler);
-
-  while (!stop)
-  {
-    conn_fd = accept(sock_fd, (struct sockaddr *)&cln_addr, &sLen);
-    if (conn_fd == -1)
-    {
-      perror("Error: accept()");
-      continue;
-    }
-    if (pthread_create(&thread, NULL, (void *(*)(void *)) & connectCallback, (void *)conn_fd))
-    {
-      perror("Error: pthread_create()\n");
-    }
-    // connectCallback(conn_fd, &total_amount);
-  }
-  printf("closing sock_fd\n");
-  close(sock_fd);
-  return 0;
-}
-
-void connectCallback(int conn_fd)
-{
-  char rcv[BUFSIZ];
-  int n;
-  
-  while ((n = read(conn_fd, rcv, BUFSIZE)) != 0)
-  {
-    // printf("%.*s\n", n, rcv); // print string with given length
-    P(sem);
-    if (rcv[0] == 'D')
-    {
-      // printf("+%d\n", atoi(rcv + 2));
-      total_amount += atoi(rcv + 2);
-      printf("after deposit: %d\n", total_amount);
-    }
-    else if (rcv[0] == 'W')
-    {
-      // printf("-%d\n", atoi(rcv + 2));
-      total_amount -= atoi(rcv + 2);
-      printf("after withdraw: %d\n", total_amount);
-    }
-    V(sem);
-    // printf("size = %d\n", n);
-    // printf("balance = %d\n", total_amount);
-  }
-
-  // printf("exit\n");
-  close(conn_fd);
-  pthread_exit(NULL);
-  return;
-}
+  long t; //thread number
+  int connfd; //socket connfd
+} info_transfer;
 
 int P(int s){
   struct sembuf sop;
@@ -118,7 +33,8 @@ int P(int s){
   if(semop(s, &sop, 1) < 0){
     perror("P(): sem failed\n");
     return -1;
-  }else{
+  }
+  else{
     return 0;
   }
 }
@@ -132,21 +48,142 @@ int V(int s){
   if(semop(s, &sop, 1) < 0){
     perror("V(): sem failed\n");
     return -1;
-  }else{
+  }
+  else{
     return 0;
   }
 }
 
-void intHandler(int signum)
+void intHandler()
 {
-
-  stop = 1;
-  printf("closing sock_fd\n");
-  close(sock_fd);
-
   printf("remove sem\n");
   if(semctl(sem, 0, IPC_RMID, 0) < 0){
     perror("Error removing sem\n");
     exit(-1);
   }
+
+  exit(0);
+}
+
+void *connectCallback(void *info)
+{
+  info_transfer *my_info = (info_transfer *)info;
+  int n, index = 0;
+  char rcv[2], str[20];
+
+  printf("create thread %ld\n", my_info->t);
+
+  while (1)
+  {
+    if((n = read(my_info->connfd, rcv, 1)) == -1){
+      printf("Error: read()\n");
+      break;
+    }
+
+    if(n == 0){
+      break;
+    }
+
+    int value=0;
+    if(rcv[0]!='\0'){
+      strncat(str, &rcv[0], 1);
+    }
+    else{
+      if(str[0] == 'w'){
+        int j = 9;
+        int w_value=0;
+        while(str[j] != '\0'){
+          w_value = w_value * 10 + (int)(str[j] - '0');
+          j++;
+        }
+
+        value = -w_value;
+
+        memset(str, 0, 20);
+        P(s);
+        amount += value;
+
+        printf("After withdraw: %d\n", amount);
+        V(s);
+      }
+      else if(str[0] == 'd'){
+        int j = 8;
+        int d_value = 0;
+        while(str[j] != '\0'){
+          d_value = d_value * 10 + (int)(str[j] - '0');
+          j++;
+        }
+        value = d_value;
+
+        memset(str, 0, 20);
+        P(s);
+        amount += value;
+
+        printf("After deposit: %d\n", amount);
+        V(s);
+      }
+
+      index++;
+    }
+  }
+
+  close(my_info->connfd);
+  pthread_exit((void *)0);
+}
+
+int main(int argc, char **argv)
+{
+  signal(SIGINT, intHandler);
+
+  int sockfd, connfd, rc;
+  long t = 0;
+  struct sockaddr_in cln_addr;
+  socklen_t sLen = sizeof(cln_addr);
+  pthread_t thread[NUM_THREADS];
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+  if (argc != 2){
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(-1);
+  }
+
+  sockfd = createServerSock(atoi(argv[1]), TRANSPORT_TYPE_TCP);
+  if(sockfd < 0){
+    printf("Error create socket\n");
+    exit(-1);
+  }
+
+  // Creating semaphore
+  sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | SEM_MODE);
+  if (sem < 0){
+    fprintf (stderr , "%s : creation of semaphore %d failed : %s\n" , argv [0] , key , strerror (errno) ); 
+    exit (1); 
+  }
+  printf("sem created\n");
+  semctl(s , 0, SETVAL, 1);
+
+  info_transfer info[NUM_THREADS];
+  while (1)
+  {
+    /* waiting for connection */
+    connfd = accept(sockfd, (struct sockaddr *)&cln_addr, &sLen);
+    if (connfd == -1){
+      printf("Error : accept ()\n");
+      break;
+    }
+
+    info[t].connfd = connfd; //the new thread's connfd
+    info[t].t = t;           //tread number
+
+    rc = pthread_create(&thread[t], &attr, connectCallback, (void *)&info[t]);
+    if(rc){
+      printf("ERROR: return code from pthread_create() is %d\n", rc);
+      exit(-1);
+    }
+
+    t++;
+  }
+  return 0;
 }
