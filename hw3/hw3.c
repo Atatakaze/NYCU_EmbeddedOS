9d45944ee9dc3afb4bc76e382d70da87a6ef082e
bobo <bobo511326@gmail.com>
2022-12-29 16:09:52 +0800

    hw3 ver1

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -5,13 +5,12 @@
 #include <signal.h>  // signal()
 #include <sys/ipc.h>
 #include <sys/shm.h> //for shared memory
-
 #include <sys/wait.h>
 #include <sys/sem.h>
+
 #include "socket_utils.h"
 
 #define BUFSIZE 1024
-#define NUM_THREADS 10
 
 #define SEM_KEY 1234
 #define SEM_MODE 0666
@@ -27,38 +26,53 @@ typedef struct AREA{
 } AREA;
 
 int sem, processNumber=0;
-pid_t childpid;
+int sockfd, connfd;
 
 /* shm parameter */
 AREA *area;
 int shmid;
-key_t key;
+key_t key = 4321;
 
 /* ================================================ */
-/*                  share memory                    */
+/*                  signal                          */
 /* ================================================ */
-void shmHandler() {
-    /* Detach the share memory segment */
-    shmdt(area);
-    
+void intHandler() 
+{
+    /* close client connection */
+    close(connfd);
+    /* close server socket */
+    close(sockfd);
+
     /* Destroy the share memory segment */
-    printf ("[INFO] Destroy the share memory.\n");
-
     int retval;
     retval = shmctl(shmid, IPC_RMID, NULL);
-    if ( retval < 0)
-    {
-        fprintf (stderr , "[ERROR]: Remove share memory failed.\n");
+    if ( retval < 0){
+        fprintf(stderr , "[ERROR]: Remove share memory failed.\n");
         exit (1);
     }
-    exit(1);
+    printf ("[INFO] Destroy the share memory.\n");
+
+    /* Destroy semaphore */
+    if(semctl(sem, 0, IPC_RMID, 0) < 0){
+        perror("[ERROR]: Fail to remove semaphore %d.\n", SEM_KEY);
+        exit(1);
+    }
+    printf("[INFO]: Remove semaphore %d.\n", SEM_KEY);
+
+    exit(0);
+}
+
+void childHandler()
+{
+    while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 
 /* ================================================ */
 /*                  Semaphore                       */
 /* ================================================ */
-int P(int s){
+int P(int s)
+{
   struct sembuf sop;
   sop.sem_num = 0;
   sop.sem_op = -1;
@@ -73,7 +87,8 @@ int P(int s){
   }
 }
 
-int V(int s){
+int V(int s)
+{
   struct sembuf sop;
   sop.sem_num = 0;
   sop.sem_op = 1;
@@ -88,46 +103,29 @@ int V(int s){
   }
 }
 
-void semHandler()
-{
-  printf("[INFO]: Remove semaphore.\n");
-  if(semctl(sem, 0, IPC_RMID, 0) < 0){
-    perror("[ERROR]: Fail to remove sem.\n");
-    exit(-1);
-  }
-
-  exit(0);
-}
-
 /* ================================================ */
 /*                  process                         */
 /* ================================================ */
 void childProcess(info_transfer info)
 {
     int i, n, substr_count = 0, which_area, n_report, n_case;
-    char transmit_buf[BUFSIZE], receive_buf[BUFSIZE], buf[BUFSIZE];
+    char transmit_buf[BUFSIZE], receive_buf[BUFSIZE];
     char* s;                        // use to seperate each command by '|' and ' '
     char* substr[30];               // store sub string
-
-    // share memory attach
-    if ((shmid = shmget(key, sizeof(AREA), 0666)) < 0) {
-        perror("[ERROR]: Fail when shmget()");
-        exit(1);
-    }
-    /* atach the segment to our data space */
-    if ((area = shmat(shmid, NULL, 0)) == (AREA *) -1) {
-        perror("[ERROR]: Fail when shmat()");
-        exit(1);
-    }
-    //printf("[Process %d]: Attach to the share memory.\n", info.number);
+    AREA myArea;
 
     while (1)
     {
+        /* read message from client */
         memset(receive_buf, 0, BUFSIZE);
         if((n = read(info.connfd, receive_buf, BUFSIZE)) == -1){
             perror("[ERROR]: read()\n");
         }
-        printf(" > [Process %d]: Receive %s\n", info.number, receive_buf);
+        if((strcmp(receive_buf, " ") == 0) || n == 0){
+            printf("[INFO](Process %d): End process.\n", info.number);
+            break;
+        }
+        printf(" > [Receive](process %d): %s\n", info.number, receive_buf);
 
         /* seperate command by the '|' and ' ' */
         substr_count = 0;
@@ -150,95 +148,149 @@ void childProcess(info_transfer info)
         /* command: Confirmed case (return infomation of confirmed cases) */
         if(strcmp(substr[0], "Confirmed") == 0){
             /* command: Confirmed case */
-            memset(transmit_buf, 0, BUFSIZE);
-            P(sem);
+            char temp[100] = {0};
+            
             if(substr_count == 2){
-                n = sprintf(transmit_buf, "0 : %d\n1 : %d\n2 : %d\n3 : %d\n4 : %d\n5 : %d\n6 : %d\n7 : %d\n8 : %d\n", 
+                P(sem);
+                // attach share memory
+                if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1){
+                    printf("[ERROR](Process %d): Fail to attach share memory.", info.number);
+                    exit(1);
+                }
+
+                n = sprintf(temp, "0 : %d\n1 : %d\n2 : %d\n3 : %d\n4 : %d\n5 : %d\n6 : %d\n7 : %d\n8 : %d\n", 
                             (area->mild[0] + area->severe[0]), (area->mild[1] + area->severe[1]), (area->mild[2] + area->severe[2]),
                             (area->mild[3] + area->severe[3]), (area->mild[4] + area->severe[4]), (area->mild[5] + area->severe[5]), 
                             (area->mild[6] + area->severe[6]), (area->mild[7] + area->severe[7]), (area->mild[8] + area->severe[8]));
+
+                shmdt(area);
+                V(sem);
             }
             /* command: Confirmed case | Area x */
             else{
+                P(sem);
+                // attach share memory
+                if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1){
+                    printf("[ERROR](Process %d): Fail to attach share memory.", info.number);
+                    exit(1);
+                }
+
                 which_area = atoi(substr[3]);
-                n = sprintf(transmit_buf, "Area %d - Mild : %d | Severe : %d\n", which_area, area->mild[which_area], area->severe[which_area]);
+                n = sprintf(temp, "Area %d - Mild : %d | Severe : %d\n", which_area, area->mild[which_area], area->severe[which_area]);
+                
+                shmdt(area);
+                V(sem);
             }
 
+            memset(transmit_buf, 0, BUFSIZE);
+            strcpy(transmit_buf, temp);
             if((n = write(info.connfd, transmit_buf, n)) == -1){
                 perror("Error: write()\n");
             }
-            V(sem);
-
-            printf(" > [Process %d]: Transmit\n%s\n", info.number, transmit_buf);
+            printf(" > [Transmit](Process %d): Transmit (length %d)\n%s\n", info.number, n, transmit_buf);
         }
         /* command: Reporting system */
         if(strcmp(substr[0], "Reporting") == 0){
             /* command: Reporting sytem | Area x | Mild/Severe x */
+            char temp = {0};
+            char wait = {0};
+
+            n = sprintf(wait, "The ambulance is on it's way...\n");
             memset(transmit_buf, 0, BUFSIZE);
-            P(sem);
-            n = sprintf(transmit_buf, "The ambulance is on it's way...\n");
+            strcpy(transmit_buf, wait);
             if((n = write(info.connfd, transmit_buf, n)) == -1){
                 perror("Error: write()\n");
             }
-            printf(" > [Process %d]: Transmit\n%s\n", info.number, transmit_buf);
+            printf(" > [Transmit](Process %d): %s\n", info.number, transmit_buf);
 
-            n_report = ((substr_count - 2) / 4);
-            for(i = 0; i < n_report; i++){
-                which_area = atoi(substr[(3 + 4 * i)]);
-                n_case = atoi(substr[(5 + 4 * i)]);
-                if(strcmp(substr[(4 + 4 * i)], "Mild") == 0){
-                    area->mild[which_area] += n_case;
+            // initialize the local case report
+            for(i = 0; i < 9; i++){
+                myArea.mild[i] = 0;
+                myArea.severe[i] = 0;
+            }
+
+            for(i = 2; i < substr_count; i++){
+                if(strcmp(substr[i], "Area") == 0){
+                    which_area = atoi(substr[(i + 1)]);
+                }
+                if(strcmp(substr[i], "Mild") == 0){
+                    n_case = atoi(substr[(i + 1)]);
+                    myArea.mild[which_area] += n_case;
+                }
+                if(strcmp(substr[i], "Severe") == 0){
+                    n_case = atoi(substr[(i + 1)]);
+                    myArea.severe[which_area] += n_case;
+                }
+            }
+
+            which_area = 8;
+            while(i >= 0){
+                if((myArea.mild[which_area] + myArea.severe[which_area]) != 0){
+                    break;
                 }
                 else{
-                    area->severe[which_area] += n_case;
+                    which_area -= 1;
                 }
             }
 
-            for(i = 0; i < n_report; i++){
-                if(which_area < atoi(substr[(3 + 4 * i)])){
-                    which_area = atoi(substr[(3 + 4 * i)]);
-                }
-            }
-            V(sem);
-
+            printf(" > [INFO](Process %d): Wait %d seconds...\n", info.number, which_area);
             sleep(which_area);
 
-            memset(buf, 0, BUFSIZE);
-            for(i = 0; i < n_report; i++){
-                strcat(buf, "Area ");
-                strcat(buf, substr[(3 + 4 * i)]);
-                strcat(buf, " | ");
-                strcat(buf, substr[(4 + 4 * i)]);
-                strcat(buf, " ");
-                strcat(buf, substr[(5 + 4 * i)]);
-                strcat(buf, "\n");
+            // update local report to share memory
+            P(sem);
+            // attach share memory
+            if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1){
+                printf("[ERROR](Process %d): Fail to attach share memory.", info.number);
+                exit(1);
+            }
+            
+            for(i = 0; i < 9; i++){
+                area->mild[i] += myArea.mild[i];
+                area->severe[i] += myArea.severe[i];
+            }
+            shmdt(area);
+            V(sem);
+
+            memset(transmit_buf, 0, BUFSIZE);
+            for(i = 0; i < 9; i++){
+                if((myArea.mild[i] + myArea.severe[i]) != 0){
+                    memset(temp, 0, 100);
+                    if((myArea.mild[i] != 0) && (myArea.severe[i] == 0)){
+                        sprintf(temp, "Area %d | Mild %d\n", i, myArea.mild[i]);
+                    }
+                    if((myArea.mild[i] == 0) && (myArea.severe[i] != 0)){
+                        sprintf(temp, "Area %d | Severe %d\n", i, myArea.severe[i]);
+                    }
+                    if((myArea.mild[i] != 0) && (myArea.severe[i] != 0)){
+                        sprintf(temp, "Area %d | Severe %d | Mild %d\n", i, myArea.severe[i], myArea.mild[i]);
+                    }
+                    strcat(transmit_buf, temp);
+                }
             }
 
-            if((n = write(info.connfd, buf, strlen(buf))) == -1){
+            if((n = write(info.connfd, transmit_buf, strlen(transmit_buf))) == -1){
                 perror("Error: write()\n");
             }
-            printf(" > [Process %d]: Transmit\n%s\n", info.number, buf);
+            printf(" > [Transmit](Process %d): \n%s\n", info.number, transmit_buf);
         }
         /* command: Exit (exit system) */
         if(strcmp(substr[0], "Exit") == 0){
             break;
         }
     }
+    memset(receive_buf, 0, BUFSIZE);
     close(info.connfd);
     exit(0);
 }
 
-void childHandler(int signum)
-{
-    while (waitpid(-1, NULL, WNOHANG) > 0);
-}
-
+/* ================================================ */
+/*                     main                         */
+/* ================================================ */
 
 int main(int argc, char *argv[])
 {
-    int sockfd, connfd, i;
-
-    /* socket */
+    int i;
+    pid_t childpid;
     struct sockaddr_in cln_addr;
     socklen_t sLen = sizeof(cln_addr);
 
@@ -254,21 +306,19 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    // Creating semaphore
+    /* create semaphore */
     sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | SEM_MODE);
     if (sem < 0){
         fprintf (stderr , "%s : creation of semaphore %d failed : %s\n" , argv [0] , SEM_KEY , strerror (errno) ); 
         exit (1); 
     }
-    printf("[INFO] sem created.\n");
     semctl(sem , 0, SETVAL, 1);
+    printf("[INFO] Create semaphore %d.\n", SEM_KEY);
 
-    signal(SIGINT, semHandler);
+    signal(SIGINT, intHandler);
     signal(SIGCHLD, childHandler);
-    signal(SIGINT, shmHandler);
 
     /* Create the segment */
-    key = 4321;
     if ((shmid = shmget(key ,sizeof(AREA), IPC_CREAT | 0666)) < 0) {
         perror("shmget");
         exit (1);
@@ -278,6 +328,7 @@ int main(int argc, char *argv[])
         perror("shmat");
         exit (1);
     }
+    printf("[INFO] Create and attach share memory.\n");
 
     /* initialization of infomation of 8 area */
     for(i = 0; i < 9; i++){
