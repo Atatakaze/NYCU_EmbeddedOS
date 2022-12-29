d28b70ba7f7eaab3a13082f863cd78c42bfcb6c1
bobo <bobo511326@gmail.com>
2022-12-30 02:25:39 +0800

    hw3 complete

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -12,7 +12,7 @@
 
 #define BUFSIZE 1024
 
-#define SEM_KEY 1234
+#define SEM_KEY 123456
 #define SEM_MODE 0666
 
 typedef struct{
@@ -36,7 +36,7 @@ key_t key = 4321;
 /* ================================================ */
 /*                  signal                          */
 /* ================================================ */
-void intHandler() 
+void interrupt_handler() 
 {
     /* close client connection */
     close(connfd);
@@ -47,24 +47,26 @@ void intHandler()
     int retval;
     retval = shmctl(shmid, IPC_RMID, NULL);
     if ( retval < 0){
-        fprintf(stderr , "[ERROR]: Remove share memory failed.\n");
-        exit (1);
+        perror("[ERROR]: Remove share memory failed.\n");
+        exit (-1);
     }
     printf ("[INFO] Destroy the share memory.\n");
 
     /* Destroy semaphore */
-    if(semctl(sem, 0, IPC_RMID, 0) < 0){
+    int s = semget(SEM_KEY, 1, 0);
+    if(s >= 0){
+        if(semctl(sem, 0, IPC_RMID, 0) < 0){
         perror("[ERROR]: Fail to remove semaphore.\n");
-        exit(1);
+        exit(-1);
+        }
     }
     printf("[INFO]: Remove semaphore %d.\n", SEM_KEY);
-
     exit(0);
 }
 
-void childHandler()
+void child_handler()
 {
-    while (waitpid(-1, NULL, WNOHANG) > 0);
+    while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 
@@ -109,17 +111,27 @@ int V(int s)
 void childProcess(info_transfer info)
 {
     int i, n, substr_count = 0, which_area, n_case;
-    char transmit_buf[BUFSIZE], receive_buf[BUFSIZE];
+    char transmit_buf[BUFSIZE], receive_buf[BUFSIZE], ambulance[33], area_case[50], confirm[70], temp[100];
     char* s;                        // use to seperate each command by '|' and ' '
     char* substr[30];               // store sub string
-    AREA myArea;
+    AREA *area_pointer;
+    AREA myArea, local_area;
+
+    n = sprintf(ambulance, "The ambulance is on it's way...\n");
+    if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1){
+        printf("[ERROR](Process %d): Fail to attach share memory.", info.number);
+        exit(-1);
+    }
+    area_pointer = area;
 
     while (1)
     {
         /* read message from client */
         memset(receive_buf, 0, BUFSIZE);
+        memset(transmit_buf, 0, BUFSIZE);
         if((n = read(info.connfd, receive_buf, BUFSIZE)) == -1){
             perror("[ERROR]: read()\n");
+            exit(-1);
         }
         if((strcmp(receive_buf, " ") == 0) || n == 0){
             printf("[INFO](Process %d): End process.\n", info.number);
@@ -135,78 +147,64 @@ void childProcess(info_transfer info)
             s = strtok(NULL, " |");
         }
 
+        P(sem);
+        for(i = 0; i < 9; i++){
+            myArea.mild[i] = area_pointer->mild[i];
+            myArea.severe[i] = area_pointer->severe[i];
+        }
+
         /* command: list (return categories) */
         if(strcmp(substr[0], "list") == 0){
+            V(sem);
             memset(transmit_buf, 0, BUFSIZE);
             n = sprintf(transmit_buf, "1. Confirmed case\n2. Reporting system\n3. Exit\n");
             if((n = write(info.connfd, transmit_buf, n)) == -1){
                 perror("Error: write()\n");
+                exit(-1);
             }
 
             printf(" > [Process %d]: Transmit\n%s\n", info.number, transmit_buf);
         }
         /* command: Confirmed case (return infomation of confirmed cases) */
+        memset(transmit_buf, 0, BUFSIZE);
         if(strcmp(substr[0], "Confirmed") == 0){
             /* command: Confirmed case */
-            char temp[100] = {0};
-            
             if(substr_count == 2){
-                P(sem);
-                // attach share memory
-                if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1){
-                    printf("[ERROR](Process %d): Fail to attach share memory.", info.number);
-                    exit(1);
+                memset(confirm, 0, 70);
+                n = sprintf(confirm, "0 : %d\n1 : %d\n2 : %d\n3 : %d\n4 : %d\n5 : %d\n6 : %d\n7 : %d\n8 : %d\n", 
+                            (myArea.mild[0] + myArea.severe[0]), (myArea.mild[1] + myArea.severe[1]), (myArea.mild[2] + myArea.severe[2]),
+                            (myArea.mild[3] + myArea.severe[3]), (myArea.mild[4] + myArea.severe[4]), (myArea.mild[5] + myArea.severe[5]), 
+                            (myArea.mild[6] + myArea.severe[6]), (myArea.mild[7] + myArea.severe[7]), (myArea.mild[8] + myArea.severe[8]));
+                strcpy(transmit_buf, confirm);
+                if((write(info.connfd, transmit_buf, 70)) == -1){
+                    perror("Error: write()\n");
+                    exit(-1);
                 }
-
-                n = sprintf(temp, "0 : %d\n1 : %d\n2 : %d\n3 : %d\n4 : %d\n5 : %d\n6 : %d\n7 : %d\n8 : %d\n", 
-                            (area->mild[0] + area->severe[0]), (area->mild[1] + area->severe[1]), (area->mild[2] + area->severe[2]),
-                            (area->mild[3] + area->severe[3]), (area->mild[4] + area->severe[4]), (area->mild[5] + area->severe[5]), 
-                            (area->mild[6] + area->severe[6]), (area->mild[7] + area->severe[7]), (area->mild[8] + area->severe[8]));
-
-                shmdt(area);
+                printf(" > [Transmit](Process %d): Transmit (length %d)\n%s\n", info.number, n, transmit_buf);
                 V(sem);
             }
             /* command: Confirmed case | Area x */
             else{
-                P(sem);
-                // attach share memory
-                if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1){
-                    printf("[ERROR](Process %d): Fail to attach share memory.", info.number);
-                    exit(1);
-                }
-
+                usleep(500);
+                memset(area_case, 0, 50);
                 which_area = atoi(substr[3]);
-                n = sprintf(temp, "Area %d - Mild : %d | Severe : %d\n", which_area, area->mild[which_area], area->severe[which_area]);
-                
-                shmdt(area);
-                V(sem);
+                n = sprintf(area_case, "Area %d - Severe : %d | Mild : %d\n", which_area, area->severe[which_area], area->mild[which_area]);
+                strcpy(transmit_buf, area_case);
+                if((write(info.connfd, transmit_buf, 50)) == -1){
+                    perror("Error: write()\n");
+                    exit(-1);
+                }
+                printf(" > [Transmit](Process %d):\n%s\n", info.number, transmit_buf);
+                V(sem);   
             }
-
-            memset(transmit_buf, 0, BUFSIZE);
-            strcpy(transmit_buf, temp);
-            if((n = write(info.connfd, transmit_buf, n)) == -1){
-                perror("Error: write()\n");
-            }
-            printf(" > [Transmit](Process %d): Transmit (length %d)\n%s\n", info.number, n, transmit_buf);
         }
         /* command: Reporting system */
         if(strcmp(substr[0], "Reporting") == 0){
             /* command: Reporting sytem | Area x | Mild/Severe x */
-            char temp[100] = {0};
-            char wait[100] = {0};
-
-            n = sprintf(wait, "The ambulance is on it's way...\n");
-            memset(transmit_buf, 0, BUFSIZE);
-            strcpy(transmit_buf, wait);
-            if((n = write(info.connfd, transmit_buf, n)) == -1){
-                perror("Error: write()\n");
-            }
-            printf(" > [Transmit](Process %d): %s\n", info.number, transmit_buf);
-
             // initialize the local case report
             for(i = 0; i < 9; i++){
-                myArea.mild[i] = 0;
-                myArea.severe[i] = 0;
+                local_area.mild[i] = 0;
+                local_area.severe[i] = 0;
             }
 
             for(i = 2; i < substr_count; i++){
@@ -215,17 +213,17 @@ void childProcess(info_transfer info)
                 }
                 if(strcmp(substr[i], "Mild") == 0){
                     n_case = atoi(substr[(i + 1)]);
-                    myArea.mild[which_area] += n_case;
+                    local_area.mild[which_area] += n_case;
                 }
                 if(strcmp(substr[i], "Severe") == 0){
                     n_case = atoi(substr[(i + 1)]);
-                    myArea.severe[which_area] += n_case;
+                    local_area.severe[which_area] += n_case;
                 }
             }
 
-            which_area = 8;
-            while(i >= 0){
-                if((myArea.mild[which_area] + myArea.severe[which_area]) != 0){
+            which_area = 8; // sleep time
+            while(which_area >= 0){
+                if((local_area.mild[which_area] + local_area.severe[which_area]) != 0){
                     break;
                 }
                 else{
@@ -233,53 +231,57 @@ void childProcess(info_transfer info)
                 }
             }
 
-            printf(" > [INFO](Process %d): Wait %d seconds...\n", info.number, which_area);
-            sleep(which_area);
-
-            // update local report to share memory
-            P(sem);
-            // attach share memory
-            if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1){
-                printf("[ERROR](Process %d): Fail to attach share memory.", info.number);
-                exit(1);
-            }
-            
-            for(i = 0; i < 9; i++){
-                area->mild[i] += myArea.mild[i];
-                area->severe[i] += myArea.severe[i];
-            }
-            shmdt(area);
-            V(sem);
-
             memset(transmit_buf, 0, BUFSIZE);
             for(i = 0; i < 9; i++){
-                if((myArea.mild[i] + myArea.severe[i]) != 0){
+                if((local_area.mild[i] + local_area.severe[i]) != 0){
                     memset(temp, 0, 100);
-                    if((myArea.mild[i] != 0) && (myArea.severe[i] == 0)){
-                        sprintf(temp, "Area %d | Mild %d\n", i, myArea.mild[i]);
+                    if((local_area.mild[i] != 0) && (local_area.severe[i] == 0)){
+                        sprintf(temp, "Area %d | Mild %d\n", i, local_area.mild[i]);
                     }
-                    if((myArea.mild[i] == 0) && (myArea.severe[i] != 0)){
-                        sprintf(temp, "Area %d | Severe %d\n", i, myArea.severe[i]);
+                    if((local_area.mild[i] == 0) && (local_area.severe[i] != 0)){
+                        sprintf(temp, "Area %d | Severe %d\n", i, local_area.severe[i]);
                     }
-                    if((myArea.mild[i] != 0) && (myArea.severe[i] != 0)){
-                        sprintf(temp, "Area %d | Severe %d | Mild %d\n", i, myArea.severe[i], myArea.mild[i]);
+                    if((local_area.mild[i] != 0) && (local_area.severe[i] != 0)){
+                        sprintf(temp, "Area %d | Severe %d | Mild %d\n", i, local_area.severe[i], local_area.mild[i]);
                     }
                     strcat(transmit_buf, temp);
                 }
             }
 
-            if((n = write(info.connfd, transmit_buf, strlen(transmit_buf))) == -1){
-                perror("Error: write()\n");
+            for(i = 0; i < 9; i++){
+                myArea.mild[i] += local_area.mild[i];
+                area_pointer->mild[i] = myArea.mild[i];
+                myArea.severe[i] += local_area.severe[i];
+                area_pointer->severe[i] = myArea.severe[i];
             }
-            printf(" > [Transmit](Process %d): \n%s\n", info.number, transmit_buf);
+            V(sem);
+
+            usleep(500);
+            printf(" > [INFO](Process %d): Wait %d seconds...\n", info.number, which_area);
+            if((write(info.connfd, ambulance, 33)) == -1){
+                perror("Error: write()\n");
+                exit(-1);
+            }
+            printf(" > [Transmit](Process %d): %s\n", info.number, ambulance);
+            
+            sleep(which_area);
+
+            if((write(info.connfd, transmit_buf, 90)) == -1){
+                perror("Error: write()\n");
+                exit(-1);
+            }
+            printf(" > [Transmit](Process %d): %s\n", info.number, transmit_buf);
+            usleep(500);    
         }
         /* command: Exit (exit system) */
         if(strcmp(substr[0], "Exit") == 0){
+            V(sem);
+            shmdt(area);
+            close(info.connfd);
             break;
         }
     }
     memset(receive_buf, 0, BUFSIZE);
-    close(info.connfd);
     exit(0);
 }
 
@@ -293,10 +295,18 @@ int main(int argc, char *argv[])
     pid_t childpid;
     struct sockaddr_in cln_addr;
     socklen_t sLen = sizeof(cln_addr);
+    AREA *area_pointer;
 
     if (argc != 2){
         printf("[ERROR]: Usage: %s <port>\n", argv[0]);
         exit(-1);
+    }
+
+    if(signal(SIGINT, interrupt_handler) == SIG_ERR){
+        printf("[ERROR]: Failed to catch SIGINT signal.\n");
+    }
+    if(signal(SIGCHLD, child_handler) == SIG_ERR){
+        printf("[ERROR]: Failed to catch SIGCHID signal.\n");
     }
 
     /* create socket server */
@@ -308,32 +318,33 @@ int main(int argc, char *argv[])
 
     /* create semaphore */
     sem = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | SEM_MODE);
-    if (sem < 0){
+    if(sem < 0){
         fprintf (stderr , "%s : creation of semaphore %d failed : %s\n" , argv [0] , SEM_KEY , strerror (errno) ); 
-        exit (1); 
+        exit (-1); 
     }
-    semctl(sem , 0, SETVAL, 1);
+    if(semctl(sem , 0, SETVAL, 1) < 0){
+        perror("[ERROR]: Unable to set initial value 1 to semaphore.");
+        exit(-1);
+    }
     printf("[INFO] Create semaphore %d.\n", SEM_KEY);
-
-    signal(SIGINT, intHandler);
-    signal(SIGCHLD, childHandler);
 
     /* Create the segment */
     if ((shmid = shmget(key ,sizeof(AREA), IPC_CREAT | 0666)) < 0) {
         perror("shmget");
-        exit (1);
+        exit(-1);
     }
     /* Attach the segment */    
     if ((area = (AREA *)shmat(shmid, NULL, 0)) == (AREA *) -1) {
         perror("shmat");
-        exit (1);
+        exit (-1);
     }
     printf("[INFO] Create and attach share memory.\n");
 
+    area_pointer = area;
     /* initialization of infomation of 8 area */
     for(i = 0; i < 9; i++){
-        area->mild[i] = 0;
-        area->severe[i] = 0;
+        area_pointer->mild[i] = 0;
+        area_pointer->severe[i] = 0;
     }
 
     info_transfer info;
@@ -367,6 +378,5 @@ int main(int argc, char *argv[])
 
         processNumber++;
     }
-    close(sockfd);
     return 0;
 }
